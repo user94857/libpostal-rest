09b12137c7ebfa4c2fdf8706066632b91d0b93dd
user94857 <softgamerequest@hotmail.com>
2018-08-06 21:34:05 -0400

    can enter plain unformatted address in post body
    also added a get request alternate. can't use spaces though, best to replace with commas like 101,main,st

--- main.go
+++ main.go
@@ -1,92 +1,119 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
-
-	"github.com/gorilla/mux"
-	expand "github.com/openvenues/gopostal/expand"
-	parser "github.com/openvenues/gopostal/parser"
+        "context"
+        "encoding/json"
+        "fmt"
+        "io/ioutil"
+        "net/http"
+        "os"
+        "os/signal"
+        "time"
+        "github.com/gorilla/mux"
+        expand "github.com/openvenues/gopostal/expand"
+        parser "github.com/openvenues/gopostal/parser"
 )
 
-type Request struct {
-	Query string `json:"query"`
-}
-
 func main() {
-	host := os.Getenv("LISTEN_HOST")
-	if host == "" {
-		host = "0.0.0.0"
-	}
-	port := os.Getenv("LISTEN_PORT")
-	if port == "" {
-		port = "8080"
-	}
-	listenSpec := fmt.Sprintf("%s:%s", host, port)
+        host := os.Getenv("LISTEN_HOST")
+        if host == "" {
+                host = "0.0.0.0"
+        }
+        port := os.Getenv("LISTEN_PORT")
+        if port == "" {
+                port = "8080"
+        }
+        listenSpec := fmt.Sprintf("%s:%s", host, port)
 
-	certFile := os.Getenv("SSL_CERT_FILE")
-	keyFile := os.Getenv("SSL_KEY_FILE")
+        certFile := os.Getenv("SSL_CERT_FILE")
+        keyFile := os.Getenv("SSL_KEY_FILE")
 
-	router := mux.NewRouter()
-	router.HandleFunc("/health", HealthHandler).Methods("GET")
-	router.HandleFunc("/expand", ExpandHandler).Methods("POST")
-	router.HandleFunc("/parser", ParserHandler).Methods("POST")
+        router := mux.NewRouter()
+        router.HandleFunc("/health", HealthHandler).Methods("GET")
+        router.HandleFunc("/parser", ParserUrlHandler).Queries("address", "{address}").Methods("GET")
+        router.HandleFunc("/parser", ParserParamHandler).Queries("language", "{language}", "country", "{country}").Methods("POST")
+        router.HandleFunc("/parser", ParserParamHandler).Queries("language", "{language}").Methods("POST")
+        router.HandleFunc("/parser", ParserParamHandler).Queries("country", "{country}").Methods("POST")
+        router.HandleFunc("/parser", ParserHandler).Methods("POST")
+        router.HandleFunc("/expand", ExpandHandler).Methods("POST")
 
-	s := &http.Server{Addr: listenSpec, Handler: router}
-	go func() {
-		if certFile != "" && keyFile != "" {
-			fmt.Printf("listening on https://%s\n", listenSpec)
-			s.ListenAndServeTLS(certFile, keyFile)
-		} else {
-			fmt.Printf("listening on http://%s\n", listenSpec)
-			s.ListenAndServe()
-		}
-	}()
+        s := &http.Server{Addr: listenSpec, Handler: router}
+        go func() {
+                if certFile != "" && keyFile != "" {
+                        fmt.Printf("listening on https://%s\n", listenSpec)
+                        s.ListenAndServeTLS(certFile, keyFile)
+                } else {
+                        fmt.Printf("listening on http://%s\n", listenSpec)
+                        s.ListenAndServe()
+                }
+        }()
 
-	stop := make(chan os.Signal)
-	signal.Notify(stop, os.Interrupt)
-
-	<-stop
-	fmt.Println("\nShutting down the server...")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	s.Shutdown(ctx)
-	fmt.Println("Server stopped")
+        stop := make(chan os.Signal)
+        signal.Notify(stop, os.Interrupt)
+        <-stop
+        fmt.Println("\nShutting down the server...")
+        ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+        s.Shutdown(ctx)
+        fmt.Println("Server stopped")
 }
 
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("READY"))
+}
+
+func ParserUrlHandler(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        address := vars[("address")]
+
+        if (len(address) == 0) {
+                http.Error(w, "Address cannot be empty", 500)
+                return
+        }
+
+        parsed := parser.ParseAddress(address)
+        parseThing, _ := json.Marshal(parsed)
+
+        w.Header().Set("Content-Type", "application/json")
+        w.Write([]byte(parseThing))
 }
 
 func ExpandHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+        q, _ := ioutil.ReadAll(r.Body)
+        address := string(q[:])
+        expansions := expand.ExpandAddress(address)
+        expansionThing, _ := json.Marshal(expansions)
 
-	var req Request
-
-	q, _ := ioutil.ReadAll(r.Body)
-	json.Unmarshal(q, &req)
-
-	expansions := expand.ExpandAddress(req.Query)
-
-	expansionThing, _ := json.Marshal(expansions)
-	w.Write(expansionThing)
+        w.Header().Set("Content-Type", "application/json")
+        w.Write(expansionThing)
 }
 
 func ParserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+        q, _ := ioutil.ReadAll(r.Body)
+        address := string(q[:])
+        parsed := parser.ParseAddress(address)
+        parseThing, _ := json.Marshal(parsed)
 
-	var req Request
+        w.Header().Set("Content-Type", "application/json")
+        w.Write(parseThing)
+}
 
-	q, _ := ioutil.ReadAll(r.Body)
-	json.Unmarshal(q, &req)
+func ParserParamHandler(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        language := vars[("language")]
+        country := vars[("country")]
 
-	parsed := parser.ParseAddress(req.Query)
-	parseThing, _ := json.Marshal(parsed)
-	w.Write(parseThing)
+        q, _ := ioutil.ReadAll(r.Body)
+        address := string(q[:])
+
+        parser_options := parser.ParserOptions {
+                Language: language,
+                Country: country,
+        }
+
+        parsed := parser.ParseAddressOptions(address, parser_options)
+        parseThing, _ := json.Marshal(parsed)
+
+        w.Header().Set("Content-Type", "application/json")
+        w.Write(parseThing)
 }
